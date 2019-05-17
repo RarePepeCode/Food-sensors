f5f88a1a35bfc63ba3d4bd95e19f8fb0484c43ac
ignkuc <ignkuc@bitbucket.org>
2019-05-17 07:57:17 +0300

    Created main compoments

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,10 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './recipes/recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesCreationComponent } from './recipes/recipes-creation/recipes-creation.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CoursesListComponent } from './courses/courses-list/courses-list.component';
+import { CoursesCreationComponent } from './courses/courses-creation/courses-creation.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesCreationComponent,
+    CoursesComponent,
+    CoursesListComponent,
+    CoursesCreationComponent,
+    NavBarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ front-end/src/app/courses/courses-creation/courses-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-creation',
+  templateUrl: './courses-creation.component.html',
+  styleUrls: ['./courses-creation.component.css']
+})
+export class CoursesCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
